91a9f83262d982a9073dedd60a281e4d1a7dc54b
Zi Nguyen <zeexnguyen@gmail.com>
2017-12-14 15:17:02 +0000

    initial commit

---
+++ caesar.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    //check if argc is 2
+    if (argc != 2) {
+        printf("missing command-line argument");
+        return 1;
+    }
+
+    //turn key into an integer
+
+}
